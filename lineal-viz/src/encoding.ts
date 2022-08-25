83c452135e62ba09f983897175eb508c06ad1206
Michael Lange <dingoeatingfuzz@gmail.com>
2022-08-25 14:01:11 -0700

    Add support for static number accessors

--- lineal-viz/src/encoding.ts
+++ lineal-viz/src/encoding.ts
@@ -4,10 +4,12 @@ export class Encoding {
   field?: string;
   accessor: Accessor;
 
-  constructor(accessor: string | Accessor) {
+  constructor(accessor: string | number | Accessor) {
     if (typeof accessor === 'string') {
       this.field = accessor;
       this.accessor = (d: any) => d[accessor];
+    } else if (typeof accessor === 'number') {
+      this.accessor = () => accessor;
     } else {
       this.accessor = accessor;
     }
