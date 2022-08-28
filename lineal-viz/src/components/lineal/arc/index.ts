08e0c46f382ad38ca209874409477fcedd880c8a
Michael Lange <dingoeatingfuzz@gmail.com>
2022-08-28 15:44:02 -0700

    Arc component
    This is technically a mark in that it emits geometry, but it takes no
    encodings since it does not read from a dataset. Another component,
    Arcs, will take encodings and emit...well...multiple arcs (donuts, pies,
    etc).

---
+++ lineal-viz/src/components/lineal/arc/index.ts
@@ -0,0 +1,56 @@
+import Component from '@glimmer/component';
+import { tracked, cached } from '@glimmer/tracking';
+import { arc } from 'd3-shape';
+
+interface ArcArgs {
+  innerRadius?: number;
+  outerRadius?: number;
+  startAngle?: number | string;
+  endAngle?: number | string;
+}
+
+// An angle can be defined as a number or a string ending with 'd'
+const parseAngle = (angle: number | string): number => {
+  if (typeof angle === 'number') return angle;
+
+  const PATTERN = /(\d+)d$/;
+
+  if (!PATTERN.test(angle)) {
+    throw new Error(
+      `Could not parse string "${angle}" as degrees. To provide an angle as degrees end a string with a lower-case "d", like "180d". If the angle provided is radians, make sure to provide it as a number, like @angle={{this.angle}}`
+    );
+  }
+
+  const degrees = parseInt(angle.match(PATTERN)?.[1] ?? '0', 10);
+  return (degrees * Math.PI) / 180;
+};
+
+export default class Line extends Component<ArcArgs> {
+  @cached get startAngle(): number {
+    return parseAngle(this.args.startAngle ?? 0);
+  }
+
+  @cached get endAngle(): number {
+    return parseAngle(this.args.endAngle ?? Math.PI * 2);
+  }
+
+  @cached get arc() {
+    return arc()
+      .innerRadius(this.args.innerRadius ?? 0)
+      .outerRadius(this.args.outerRadius ?? 100)
+      .startAngle(this.startAngle)
+      .endAngle(this.endAngle);
+  }
+
+  @cached get d() {
+    // @ts-ignore
+    return this.arc();
+  }
+
+  @cached get centroid() {
+    // @ts-ignore
+    const [x, y] = this.arc.centroid();
+    console.log('Computing centroid!', x, y);
+    return { x, y };
+  }
+}
