5521354edb9777e7097a0cfa529a8603352a67aa
Michael Lange <dingoeatingfuzz@gmail.com>
2022-09-14 16:32:25 -0700

    Helper for making a fun little sine wave

---
+++ docs/app/helpers/generate-sine.js
@@ -0,0 +1,14 @@
+import { helper } from '@ember/component/helper';
+
+function* sine(length) {
+  let x = 0;
+  while (x < length) {
+    yield { x, y: (Math.sin(x) * x) / 5 };
+    x++;
+  }
+}
+
+export default helper(([length]) => {
+  const gen = sine(length);
+  return Array.from(gen);
+});
