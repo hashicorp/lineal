d00b30bfe25ddd09559534928993a3af9e593e35
Michael Lange <dingoeatingfuzz@gmail.com>
2022-09-28 15:53:18 -0700

    Pass ResizeOberserverEntry object along through Lineal::Fluid

--- test-app/tests/integration/components/lineal/fluid-test.ts
+++ test-app/tests/integration/components/lineal/fluid-test.ts
@@ -2,22 +2,30 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render, settled } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import * as sinon from 'sinon';
 
 const wait = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 module('Integration | Component | Lineal::Fluid', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('When first rendering, width and height are immediately yielded', async function (assert) {
+  test('When first rendering, width, height, and the ResizeObserverEntry are immediately yielded', async function (assert) {
+    const spy = sinon.spy();
+    this.setProperties({ spy });
+
     await render(hbs`
-      <Lineal::Fluid style='border:1px solid black; width: 100px; height: 50px' as |width height|>
+      <Lineal::Fluid style='border:1px solid black; width: 100px; height: 50px' as |width height entry|>
         <span class='output'>{{width}} {{height}}</span>
+        {{this.spy entry}}
       </Lineal::Fluid>
     `);
 
     // TODO: This should be replaced with a more precise wait based on the ResizeObserver
     await wait(100);
+
     assert.dom('.output').hasText('100 50');
+    assert.strictEqual(spy.callCount, 2);
+    assert.ok(spy.getCall(1).args[0] instanceof ResizeObserverEntry);
   });
 
   test('Changing the dimensions of the element triggers a recomputation of the width and height', async function (assert) {
