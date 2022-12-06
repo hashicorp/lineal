e98fa4c2aae00ddb2c176cb0b3c85ba8d94c8dbd
Michael Lange <dingoeatingfuzz@gmail.com>
2022-12-05 17:20:01 -0800

    Extension for the opposite of hasProperties

--- test-app/tests/qunit-extensions.ts
+++ test-app/tests/qunit-extensions.ts
@@ -1,8 +1,15 @@
 declare global {
   interface Assert {
     hasProperties(obj: Object, props: string[], message?: string): void;
+    lacksProperties(obj: Object, props: string[], message?: string): void;
   }
 }
+
+const without = (arr: string[], subset: string[]): string[] => {
+  const asSet = new Set(arr);
+  subset.forEach((str) => asSet.delete(str));
+  return Array.from(asSet);
+};
 
 export default function extend(assert: Assert) {
   assert.hasProperties = function (
@@ -22,6 +29,28 @@ export default function extend(assert: Assert) {
       message: result
         ? message || 'Has all props'
         : `Missing expected props: ${missingProps.join(', ')}`,
+    });
+  };
+
+  assert.lacksProperties = function (
+    this: Assert,
+    obj: Object,
+    props: string[],
+    message?: string
+  ) {
+    const unexpectedProps = props.filter((val) => obj.hasOwnProperty(val));
+    const keys = Object.keys(obj);
+
+    const result = unexpectedProps.length === 0;
+    this.pushResult({
+      result,
+      actual: keys,
+      expected: without(keys, props),
+      message: result
+        ? message || 'Lacks expected props'
+        : `Includes props expected to be missing: ${unexpectedProps.join(
+            ', '
+          )}`,
     });
   };
 }
