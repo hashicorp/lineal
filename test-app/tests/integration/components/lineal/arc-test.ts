a0eb51f28b466e7438648ee2d37298443240d2dd
Michael Lange <dingoeatingfuzz@gmail.com>
2022-12-05 17:21:13 -0800

    Tests for Lineal::Arc and Lineal::Arcs

--- test-app/tests/integration/components/lineal/arc-test.ts
+++ test-app/tests/integration/components/lineal/arc-test.ts
@@ -1,10 +1,78 @@
-import { module, todo } from 'qunit';
+import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import * as sinon from 'sinon';
+import { arc } from 'd3-shape';
 
 module('Integration | Component | Lineal::Arc', function (hooks) {
   setupRenderingTest(hooks);
 
-  todo('test coverage', function () {
-    // Test coverage is coming soon, but I want to get this in front of people ASAP
+  test('When given no arguments, renders a circle', async function (assert) {
+    await render(hbs`
+      <svg>
+        <Lineal::Arc />
+      </svg>
+    `);
+
+    const arcGenerator = arc()
+      .outerRadius(100)
+      .innerRadius(0)
+      .startAngle(0)
+      .endAngle(Math.PI * 2);
+
+    // @ts-ignore
+    assert.dom('path').hasAttribute('d', arcGenerator());
+  });
+
+  test('The arc can be defined using @innerRadius, @outerRadius, @cornerRadius, @padRadius, @startAngle, @endAngle, and @padAngle just like the d3 arc generator', async function (assert) {
+    await render(hbs`
+      <svg>
+        <Lineal::Arc
+          @innerRadius='10'
+          @outerRadius='250'
+          @cornerRadius='5'
+          @padRadius='8'
+          @startAngle='45d'
+          @endAngle='135d'
+          @padAngle='18d'
+        />
+      </svg>
+    `);
+
+    const arcGenerator = arc()
+      .outerRadius(250)
+      .innerRadius(10)
+      .cornerRadius(5)
+      .padRadius(8)
+      .startAngle(Math.PI / 4)
+      .endAngle((Math.PI * 3) / 4)
+      .padAngle(Math.PI / 10);
+
+    // @ts-ignore
+    assert.dom('path').hasAttribute('d', arcGenerator());
+  });
+
+  test('The arc component yields the centroid of the drawn arc', async function (assert) {
+    const spy = sinon.spy();
+    this.setProperties({ spy });
+
+    await render(hbs`
+      <svg>
+        <Lineal::Arc as |arc|>
+          {{this.spy arc.centroid}}
+        </Lineal::Arc>
+      </svg>
+    `);
+
+    const arcGenerator = arc()
+      .outerRadius(100)
+      .innerRadius(0)
+      .startAngle(0)
+      .endAngle(Math.PI * 2);
+
+    // @ts-ignore
+    const [x, y] = arcGenerator.centroid();
+    assert.deepEqual(spy.getCall(0).args[0], { x, y });
   });
 });
--- test-app/tests/integration/components/lineal/arcs-test.ts
+++ test-app/tests/integration/components/lineal/arcs-test.ts
@@ -1,10 +1,192 @@
-import { module, todo } from 'qunit';
+import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
+import { render, findAll } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import { pie, arc } from 'd3-shape';
+import * as sinon from 'sinon';
+import { ScaleOrdinal } from '@lineal-viz/lineal/scale';
+import { ArcDatum } from '@lineal-viz/lineal/components/lineal/arcs/index';
+
+interface Datum {
+  value: number;
+}
+
+const data = [{ value: 5 }, { value: 10 }, { value: 15 }, { value: 20 }];
 
 module('Integration | Component | Lineal::Arcs', function (hooks) {
   setupRenderingTest(hooks);
 
-  todo('test coverage', function () {
-    // Test coverage is coming soon, but I want to get this in front of people ASAP
+  test('Given a dataset, theta and color accessors, and a colorScale, renders a group of paths', async function (assert) {
+    const colors = ['red', 'blue', 'yellow', 'green'];
+    const scale = new ScaleOrdinal({
+      range: colors,
+    });
+
+    this.setProperties({ data, scale });
+
+    await render(hbs`
+      <svg class="test-svg">
+        <Lineal::Arcs
+          @data={{this.data}}
+          @colorScale={{this.scale}}
+          @theta="value"
+        />
+      </svg>
+    `);
+
+    const paths = findAll('path');
+    assert.strictEqual(paths.length, data.length);
+
+    const pieGenerator = pie()
+      .startAngle(0)
+      .endAngle(Math.PI * 2)
+      .padAngle(0)
+      .value((d: any) => d.value)
+      .sortValues(null);
+
+    // @ts-ignore
+    const arcs = pieGenerator(data);
+    // These defaults come from Lineal::Arc
+    const arcGenerator = arc().innerRadius(0).outerRadius(100);
+
+    paths.forEach((slice, index) => {
+      assert.dom(slice).hasAttribute('fill', colors[index] || '');
+
+      const arcData = arcs[index];
+      // @ts-ignore -- This works and is normal d3 code, type definitions are just lacking
+      assert.dom(slice).hasAttribute('d', arcGenerator(arcData));
+    });
+  });
+
+  test('Encodings can use functions as accessors', async function (assert) {
+    const theta = (d: Datum) => (d.value > 10 ? d.value / 2 : d.value);
+    this.setProperties({ data, theta });
+
+    await render(hbs`
+      <svg class="test-svg">
+        <Lineal::Arcs
+          @data={{this.data}}
+          @theta={{this.theta}}
+        />
+      </svg>
+    `);
+
+    const paths = findAll('path');
+    assert.strictEqual(paths.length, data.length);
+
+    const pieGenerator = pie()
+      .startAngle(0)
+      .endAngle(Math.PI * 2)
+      .padAngle(0)
+      .value((d: any) => d)
+      .sortValues(null);
+
+    // @ts-ignore
+    const arcs = pieGenerator([5, 10, 7.5, 10]);
+    // These defaults come from Lineal::Arc
+    const arcGenerator = arc().innerRadius(0).outerRadius(100);
+
+    paths.forEach((slice, index) => {
+      const arcData = arcs[index];
+      // @ts-ignore -- This works and is normal d3 code, type definitions are just lacking
+      assert.dom(slice).hasAttribute('d', arcGenerator(arcData));
+    });
+  });
+
+  test('When @colorScale is a string, a ScaleOrdinal with a CSSRange is automatically made', async function (assert) {
+    this.setProperties({ data });
+
+    await render(hbs`
+      <svg class="test-svg">
+        <Lineal::Arcs
+          @data={{this.data}}
+          @colorScale="reds"
+          @theta="value"
+        />
+      </svg>
+    `);
+
+    const paths = findAll('path');
+    assert.strictEqual(paths.length, data.length);
+
+    const pieGenerator = pie()
+      .startAngle(0)
+      .endAngle(Math.PI * 2)
+      .padAngle(0)
+      .value((d: any) => d.value)
+      .sortValues(null);
+
+    paths.forEach((slice, index) => {
+      assert.dom(slice).hasClass('reds');
+      assert.dom(slice).hasClass(`reds-${index + 1}`);
+      assert.dom(slice).hasClass(`reds-${data.length}-${index + 1}`);
+    });
+  });
+
+  test('The component yields structured arc data', async function (assert) {
+    const spy = sinon.spy();
+    const colors = ['red', 'blue', 'yellow', 'green'];
+    const scale = new ScaleOrdinal({
+      range: colors,
+    });
+
+    this.setProperties({ data, scale, spy });
+
+    await render(hbs`
+      <svg class="test-svg">
+        <Lineal::Arcs
+          @data={{this.data}}
+          @colorScale={{this.scale}}
+          @theta="value"
+        as |arcs|>
+        {{#each arcs as |arc|}}
+          {{this.spy arc}}
+        {{/each}}
+      </Lineal::Arcs>
+      </svg>
+    `);
+
+    assert.strictEqual(spy.callCount, data.length);
+
+    for (let t of spy.getCalls()) {
+      const arc = t.args[0] as ArcDatum;
+      assert.hasProperties(arc, [
+        'fill',
+        'data',
+        'value',
+        'index',
+        'startAngle',
+        'endAngle',
+        'padAngle',
+      ]);
+
+      assert.lacksProperties(arc, ['cssClass']);
+    }
+  });
+
+  test('When the provided color scale uses a CSSRange for the range, the arc data provides cssClass instead of fill', async function (assert) {
+    const spy = sinon.spy();
+    this.setProperties({ data, spy });
+
+    await render(hbs`
+      <svg class="test-svg">
+        <Lineal::Arcs
+          @data={{this.data}}
+          @colorScale="reds"
+          @theta="value"
+        as |arcs|>
+        {{#each arcs as |arc|}}
+          {{this.spy arc}}
+        {{/each}}
+      </Lineal::Arcs>
+      </svg>
+    `);
+
+    assert.strictEqual(spy.callCount, data.length);
+
+    for (let t of spy.getCalls()) {
+      const arc = t.args[0] as ArcDatum;
+      assert.lacksProperties(arc, ['fill']);
+    }
   });
 });
