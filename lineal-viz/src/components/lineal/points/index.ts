11627262cacef6690873c4714546cf2dadcc3dc5
Michael Lange <dingoeatingfuzz@gmail.com>
2022-12-10 15:22:49 -0800

    New Points mark
    This mark is a short hand for creating circles at x/y positions with the
    additional encodings of size (which controls radius) and color (which
    controls fill or css class).

---
+++ lineal-viz/src/components/lineal/points/index.ts
@@ -0,0 +1,103 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { cached } from '../../../cached';
+import { Accessor, Encoding } from '../../../encoding';
+import { Scale, ScaleLinear, ScaleOrdinal, ScaleSqrt } from '../../../scale';
+import CSSRange from '../../../css-range';
+import { qualifyScale } from '../../../utils/mark-utils';
+
+interface PointsArgs {
+  data: any[];
+  x: Accessor;
+  y: Accessor;
+  size: Accessor;
+  color: Accessor;
+  xScale?: Scale;
+  yScale?: Scale;
+  sizeScale?: Scale;
+  colorScale?: Scale;
+}
+
+export interface PointDatum {
+  x: number;
+  y: number;
+  size: number;
+  fill?: string;
+  cssClass?: string;
+}
+
+export default class Points extends Component<PointsArgs> {
+  @cached get x() {
+    return new Encoding(this.args.x);
+  }
+
+  @cached get y() {
+    return new Encoding(this.args.y);
+  }
+
+  @cached get size() {
+    return new Encoding(this.args.size);
+  }
+
+  @cached get color() {
+    return new Encoding(this.args.color);
+  }
+
+  @cached get xScale() {
+    const scale = this.args.xScale || new ScaleLinear();
+    qualifyScale(this, scale, this.x, 'x');
+    return scale;
+  }
+
+  @cached get yScale() {
+    const scale = this.args.yScale || new ScaleLinear();
+    qualifyScale(this, scale, this.y, 'y');
+    return scale;
+  }
+
+  @cached get sizeScale() {
+    const scale = this.args.sizeScale || new ScaleSqrt();
+    qualifyScale(this, scale, this.size, 'size');
+    return scale;
+  }
+
+  // When there is no color scale, don't color-code drawn points
+  @cached get colorScale(): Scale | undefined {
+    const scale = this.args.colorScale;
+    if (scale && this.color) {
+      if (scale instanceof Object) return scale;
+
+      return new ScaleOrdinal({
+        domain: Array.from(new Set(this.args.data.map((d) => this.color?.accessor(d)))),
+        range: new CSSRange(scale),
+      });
+    }
+  }
+
+  @cached get useCSSClass() {
+    return this.colorScale instanceof Object && this.colorScale.range instanceof CSSRange;
+  }
+
+  @cached get points(): PointDatum[] {
+    if (!this.xScale.isValid || !this.yScale.isValid || !this.sizeScale.isValid) return [];
+
+    return this.args.data.map((d: any) => {
+      const point: PointDatum = {
+        x: this.xScale.compute(this.x.accessor(d)),
+        y: this.yScale.compute(this.y.accessor(d)),
+        size: this.sizeScale.compute(this.size.accessor(d)),
+      };
+
+      if (this.colorScale) {
+        const colorValue = this.colorScale.compute(this.color.accessor(d));
+        if (this.useCSSClass) {
+          point.cssClass = colorValue;
+        } else {
+          point.fill = colorValue;
+        }
+      }
+
+      return point;
+    });
+  }
+}
