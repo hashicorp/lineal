679cee93e81e1fb78970f411ccf1e563e1b216d4
Michael Lange <dingoeatingfuzz@gmail.com>
2022-12-10 15:20:11 -0800

    Move qualifyScale into a util

---
+++ lineal-viz/src/utils/mark-utils.ts
@@ -0,0 +1,28 @@
+import { scheduleOnce } from '@ember/runloop';
+import Component from '@glimmer/component';
+import Bounds from '../bounds';
+import { Scale } from '../scale';
+import { Encoding } from '../encoding';
+
+interface MarkArgs {
+  data: any[];
+}
+
+export function qualifyScale(
+  context: Component<MarkArgs>,
+  scale: Scale,
+  encoding: Encoding,
+  field: string
+) {
+  if (scale.domain instanceof Bounds && !scale.domain.isValid) {
+    scheduleOnce('afterRender', context, () => {
+      scale.domain.qualify(context.args.data, encoding.accessor);
+    });
+  }
+
+  if (scale.range instanceof Bounds && !scale.range.isValid) {
+    throw new Error(
+      `Qualifying @${field}Scale: Cannot determine the bounds for a range without a bounding box for the mark.`
+    );
+  }
+}
