cf159c8a686e0ec373ff89ba23b144e246ecab8a
Michael Lange <dingoeatingfuzz@gmail.com>
2023-02-01 15:54:20 -0800

    API docs for parse angle

--- lineal-viz/src/utils/parse-angle.ts
+++ lineal-viz/src/utils/parse-angle.ts
@@ -3,7 +3,21 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-// An angle can be defined as a number or a string ending with 'd'
+/**
+ * Converts a well-formed degrees string into radians or passes through radians
+ * if provided with numberic input.
+ *
+ * ```
+ * console.log(parseAngle('180d'); // 3.141592653589793
+ * console.log(parseAngle(Math.PI); // 3.141592653589793
+ * ```
+ *
+ * @param angle - When a number, the number is assumed to be radians and is returned as is.
+ *                When a string, the number is parsed as degrees and converted to radians
+ *                as long as the string ends in a lower-case 'd'.
+ * @throws - When the string `angle` is malformed.
+ * @returns - The provided angle as radians.
+ */
 export default function parseAngle(angle: number | string): number {
   if (typeof angle === 'number') return angle;
 
