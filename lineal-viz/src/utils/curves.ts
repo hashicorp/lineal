210abbf2fc0f21e8749279f2da73071ac673663a
Michael Lange <dingoeatingfuzz@gmail.com>
2023-01-31 17:47:58 -0800

    Curve utils API docs

--- lineal-viz/src/utils/curves.ts
+++ lineal-viz/src/utils/curves.ts
@@ -7,6 +7,11 @@ export type CurveArgs = {
   alpha?: number;
 };
 
+/**
+ * A mapping of `string`s to D3 curve factories.
+ *
+ * This is useful for safely specifying curve functions in templates.
+ */
 export const CURVES: { [key: string]: shape.CurveFactory | shape.CurveBundleFactory } = {
   basis: shape.curveBasis,
   basisClosed: shape.curveBasisClosed,
@@ -30,6 +35,14 @@ export const CURVES: { [key: string]: shape.CurveFactory | shape.CurveBundleFact
   stepBefore: shape.curveStepBefore,
 };
 
+/**
+ * Creates a curve function from a string or argument object.
+ *
+ * @param [curve] - When a string, looks up the corresponding curve factory and returns it.
+ *                  When a `CurveArgs`, looks up the corresponding curve factory based on `curve.name`,
+ *                  applies parameters, and returns the resulting curve function.
+ * @throws {Error} - When there is no curve factory for the specified curve.
+ */
 export const curveFor = (
   curve?: string | CurveArgs
 ): shape.CurveFactory | shape.CurveBundleFactory => {
