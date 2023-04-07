2d75021e0b80ada38068ad3f1b45acfe3149542f
Michael Lange <dingoeatingfuzz@gmail.com>
2023-04-07 16:59:17 -0700

    Basic functionality of the stack transform

---
+++ lineal-viz/src/transforms/stack.ts
@@ -0,0 +1,150 @@
+import * as shape from 'd3-shape';
+import { group, merge } from 'd3-array';
+import { Accessor, Encoding } from '../encoding';
+import { tracked, cached } from '@glimmer/tracking';
+
+type OrderFn = (series: shape.Series<any, any>) => number[];
+type OffsetFn = (series: shape.Series<any, any>, order: Iterable<number>) => void;
+type Direction = 'vertical' | 'horizontal';
+type D3StackSeriesPoint = shape.SeriesPoint<{ [key: string]: number }>;
+
+export const ORDERS: { [key: string]: OrderFn } = {
+  appearance: shape.stackOrderAppearance,
+  ascending: shape.stackOrderAscending,
+  descending: shape.stackOrderDescending,
+  insideOut: shape.stackOrderInsideOut,
+  none: shape.stackOrderNone,
+  reverse: shape.stackOrderReverse,
+};
+
+export const OFFSETS: { [key: string]: OffsetFn } = {
+  expand: shape.stackOffsetExpand,
+  diverging: shape.stackOffsetDiverging,
+  none: shape.stackOffsetNone,
+  silhouette: shape.stackOffsetSilhouette,
+  wiggle: shape.stackOffsetWiggle,
+};
+
+export interface StackConfig {
+  order?: string;
+  offset?: string;
+  data: any[];
+  direction?: Direction;
+  x: Accessor | string;
+  y: Accessor | string;
+  z: Accessor | string;
+}
+
+export interface StackDatumHorizontal {
+  x0: number;
+  x1: number;
+  x: number;
+  y: any;
+  data: { [key: string]: any };
+}
+
+export interface StackDatumVertical {
+  y0: number;
+  y1: number;
+  y: number;
+  x: any;
+  data: { [key: string]: any };
+}
+
+export type StackSeriesHorizontal = StackDatumHorizontal[];
+export type StackSeriesVertical = StackDatumVertical[];
+
+export default class Stack {
+  @tracked dataIn: any[] = [];
+  @tracked order: OrderFn;
+  @tracked offset: OffsetFn;
+
+  @tracked xAccessor: Accessor | string;
+  @tracked yAccessor: Accessor | string;
+  @tracked zAccessor: Accessor | string;
+  @tracked direction: Direction;
+
+  constructor({ order, offset, data, direction, x, y, z }: StackConfig) {
+    this.offset = offset ? OFFSETS[offset] ?? OFFSETS.none : OFFSETS.none;
+    this.order = order
+      ? ORDERS[order] ?? ORDERS.none
+      : this.offset === OFFSETS.wiggle
+      ? ORDERS.insideOut
+      : ORDERS.none;
+
+    this.xAccessor = x;
+    this.yAccessor = y;
+    this.zAccessor = z;
+    this.direction = direction ?? 'vertical';
+
+    this.dataIn = data;
+  }
+
+  @cached get x() {
+    return new Encoding(this.xAccessor);
+  }
+
+  @cached get y() {
+    return new Encoding(this.yAccessor);
+  }
+
+  @cached get z() {
+    return new Encoding(this.zAccessor);
+  }
+
+  @cached get categories(): any[] {
+    return Array.from(new Set(this.dataIn.map((d) => this.z.accessor(d))));
+  }
+
+  @cached get table() {
+    // Transform from records into a table using the x-accessor as ID and the y-accessor as the cell value
+    // (or the y-accessor as ID and the x-accessor as the cell value when stacked horizontally)
+
+    const categories = this.categories;
+    const id = this.direction === 'vertical' ? this.x : this.y;
+    const cell = this.direction === 'vertical' ? this.y : this.x;
+    const idField = this.direction === 'vertical' ? 'x' : 'y';
+
+    const grouped = group(this.dataIn, id.accessor, this.z.accessor);
+
+    return Array.from(grouped).reduce((rows, [idVal, records]) => {
+      rows.push({
+        [idField]: idVal,
+        ...categories.reduce((columns, category) => {
+          // It is assumed that there is a single record per category, otherwise it would
+          // mean there were duplicte records in the source data.
+          columns[category] = cell.accessor(records.get(category)?.[0] ?? {});
+          return columns;
+        }, {}),
+      });
+      return rows;
+    }, [] as any[]);
+  }
+
+  @cached get data(): StackSeriesVertical[] | StackSeriesHorizontal[] {
+    // Convert the table of data into stacks of data
+    const d3Stack = shape.stack().keys(this.categories)(this.table);
+
+    const isVertical = this.direction === 'vertical';
+
+    const verticalMap = (d: D3StackSeriesPoint): StackDatumVertical => ({
+      y0: d[0],
+      y1: d[1],
+      y: d[1],
+      x: d.data.x,
+      data: d.data,
+    });
+    const horizontalMap = (d: D3StackSeriesPoint): StackDatumHorizontal => ({
+      x0: d[0],
+      x1: d[1],
+      x: d[1],
+      y: d.data.y,
+      data: d.data,
+    });
+
+    // Convert the [y0,y1] data format into {y0, y1} format
+    return isVertical
+      ? d3Stack.map((series) => series.map(verticalMap))
+      : d3Stack.map((series) => series.map(horizontalMap));
+  }
+}
---
+++ test-app/tests/unit/transforms/stack-test.ts
@@ -0,0 +1,156 @@
+import { module, test } from 'qunit';
+import Stack from '@lineal-viz/lineal/transforms/stack';
+
+const TEST_DATA = [
+  { day: 'Sunday', hour: 0, value: 1 },
+  { day: 'Sunday', hour: 1, value: 2 },
+  { day: 'Sunday', hour: 2, value: 1 },
+  { day: 'Sunday', hour: 3, value: 2 },
+  { day: 'Sunday', hour: 4, value: 1 },
+  { day: 'Sunday', hour: 5, value: 2 },
+  { day: 'Sunday', hour: 6, value: 1 },
+  { day: 'Sunday', hour: 7, value: 2 },
+  { day: 'Sunday', hour: 8, value: 1 },
+  { day: 'Sunday', hour: 9, value: 2 },
+  { day: 'Sunday', hour: 10, value: 1 },
+  { day: 'Sunday', hour: 11, value: 2 },
+
+  { day: 'Monday', hour: 0, value: 1 },
+  { day: 'Monday', hour: 1, value: 2 },
+  { day: 'Monday', hour: 2, value: 3 },
+  { day: 'Monday', hour: 3, value: 4 },
+  { day: 'Monday', hour: 4, value: 5 },
+  { day: 'Monday', hour: 5, value: 6 },
+  { day: 'Monday', hour: 6, value: 7 },
+  { day: 'Monday', hour: 7, value: 8 },
+  { day: 'Monday', hour: 8, value: 9 },
+  { day: 'Monday', hour: 9, value: 10 },
+  { day: 'Monday', hour: 10, value: 11 },
+  { day: 'Monday', hour: 11, value: 12 },
+
+  { day: 'Tuesday', hour: 0, value: 5 },
+  { day: 'Tuesday', hour: 1, value: 0 },
+  { day: 'Tuesday', hour: 2, value: 5 },
+  { day: 'Tuesday', hour: 3, value: 0 },
+  { day: 'Tuesday', hour: 4, value: 10 },
+  { day: 'Tuesday', hour: 5, value: 0 },
+  { day: 'Tuesday', hour: 6, value: 15 },
+  { day: 'Tuesday', hour: 7, value: 0 },
+  { day: 'Tuesday', hour: 8, value: 20 },
+  { day: 'Tuesday', hour: 9, value: 0 },
+  { day: 'Tuesday', hour: 10, value: 25 },
+  { day: 'Tuesday', hour: 11, value: 0 },
+];
+
+module('Unit | Transforms | Stack', function () {
+  test('When given an order, offset, direction, encodings, and data, a stack stacks input data', function (assert) {
+    const stack = new Stack({
+      data: TEST_DATA,
+      direction: 'vertical',
+      x: 'hour',
+      y: 'value',
+      z: 'day',
+    });
+
+    assert.deepEqual(
+      stack.table[0],
+      {
+        x: 0,
+        Sunday: 1,
+        Monday: 1,
+        Tuesday: 5,
+      },
+      'The intermediate table transformation pivots the data on the z encoding'
+    );
+
+    assert.strictEqual(
+      stack.data.length,
+      3,
+      'The stacked data contains a series for each category derived from the z encoding'
+    );
+
+    assert.deepEqual(
+      stack.data[0],
+      [
+        { y0: 0, y1: 1, y: 1, x: 0, data: stack.table[0] },
+        { y0: 0, y1: 2, y: 2, x: 1, data: stack.table[1] },
+        { y0: 0, y1: 1, y: 1, x: 2, data: stack.table[2] },
+        { y0: 0, y1: 2, y: 2, x: 3, data: stack.table[3] },
+        { y0: 0, y1: 1, y: 1, x: 4, data: stack.table[4] },
+        { y0: 0, y1: 2, y: 2, x: 5, data: stack.table[5] },
+        { y0: 0, y1: 1, y: 1, x: 6, data: stack.table[6] },
+        { y0: 0, y1: 2, y: 2, x: 7, data: stack.table[7] },
+        { y0: 0, y1: 1, y: 1, x: 8, data: stack.table[8] },
+        { y0: 0, y1: 2, y: 2, x: 9, data: stack.table[9] },
+        { y0: 0, y1: 1, y: 1, x: 10, data: stack.table[10] },
+        { y0: 0, y1: 2, y: 2, x: 11, data: stack.table[11] },
+      ],
+      'Each stack series has y0, y1, and x properties in addition to the full corresponding table record'
+    );
+
+    assert.deepEqual(
+      stack.data[1],
+      [
+        { y0: 1, y1: 2, y: 2, x: 0, data: stack.table[0] },
+        { y0: 2, y1: 4, y: 4, x: 1, data: stack.table[1] },
+        { y0: 1, y1: 4, y: 4, x: 2, data: stack.table[2] },
+        { y0: 2, y1: 6, y: 6, x: 3, data: stack.table[3] },
+        { y0: 1, y1: 6, y: 6, x: 4, data: stack.table[4] },
+        { y0: 2, y1: 8, y: 8, x: 5, data: stack.table[5] },
+        { y0: 1, y1: 8, y: 8, x: 6, data: stack.table[6] },
+        { y0: 2, y1: 10, y: 10, x: 7, data: stack.table[7] },
+        { y0: 1, y1: 10, y: 10, x: 8, data: stack.table[8] },
+        { y0: 2, y1: 12, y: 12, x: 9, data: stack.table[9] },
+        { y0: 1, y1: 12, y: 12, x: 10, data: stack.table[10] },
+        { y0: 2, y1: 14, y: 14, x: 11, data: stack.table[11] },
+      ],
+      'Each series in a stack stacks on the previous series'
+    );
+  });
+
+  test('When the direction is horizontal, series stack on x values', function (assert) {
+    const stack = new Stack({
+      data: TEST_DATA,
+      direction: 'horizontal',
+      x: 'value',
+      y: 'hour',
+      z: 'day',
+    });
+
+    assert.deepEqual(
+      stack.table[0],
+      {
+        y: 0,
+        Sunday: 1,
+        Monday: 1,
+        Tuesday: 5,
+      },
+      'The intermediate table transformation pivots the data on the z encoding'
+    );
+
+    assert.strictEqual(
+      stack.data.length,
+      3,
+      'The stacked data contains a series for each category derived from the z encoding'
+    );
+
+    assert.deepEqual(
+      stack.data[1],
+      [
+        { x0: 1, x1: 2, x: 2, y: 0, data: stack.table[0] },
+        { x0: 2, x1: 4, x: 4, y: 1, data: stack.table[1] },
+        { x0: 1, x1: 4, x: 4, y: 2, data: stack.table[2] },
+        { x0: 2, x1: 6, x: 6, y: 3, data: stack.table[3] },
+        { x0: 1, x1: 6, x: 6, y: 4, data: stack.table[4] },
+        { x0: 2, x1: 8, x: 8, y: 5, data: stack.table[5] },
+        { x0: 1, x1: 8, x: 8, y: 6, data: stack.table[6] },
+        { x0: 2, x1: 10, x: 10, y: 7, data: stack.table[7] },
+        { x0: 1, x1: 10, x: 10, y: 8, data: stack.table[8] },
+        { x0: 2, x1: 12, x: 12, y: 9, data: stack.table[9] },
+        { x0: 1, x1: 12, x: 12, y: 10, data: stack.table[10] },
+        { x0: 2, x1: 14, x: 14, y: 11, data: stack.table[11] },
+      ],
+      'Each series in a stack stacks on the previous series'
+    );
+  });
+});
