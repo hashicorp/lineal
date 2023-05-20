ceccdbb96c87e2414b3d2a2b8466aeeb753a9337
Michael Lange <dingoeatingfuzz@gmail.com>
2023-05-20 11:00:53 -0700

    Stylelint passes but I hate it a lot
    Until the Prettier CSS maintainers realize that density can be good, I
    might have to disable Prettier for CSS. Oh, and since the issue for this
    is now 4 years old, I don't actually think that will ever happen.

--- test-app/.stylelintrc.js
+++ test-app/.stylelintrc.js
@@ -2,4 +2,11 @@
 
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
+  rules: {
+    'selector-class-pattern': [
+      '^([a-z][a-z0-9]*)((--?|__)[a-z0-9]+)*$',
+      { message: 'Expected class to use kebab-case or BEM' },
+    ],
+    'declaration-block-single-line-max-declarations': 2,
+  },
 };
