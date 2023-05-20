8478727c7072a45ccae1917d47935ea350401925
Michael Lange <dingoeatingfuzz@gmail.com>
2023-05-20 10:17:13 -0700

    Upgrade test-app ember to 4.12 (latest)

--- test-app/.prettierrc.js
+++ test-app/.prettierrc.js
@@ -6,5 +6,12 @@
 'use strict';
 
 module.exports = {
-  singleQuote: true,
+  overrides: [
+    {
+      files: '*.{js,ts}',
+      options: {
+        singleQuote: true,
+      },
+    },
+  ],
 };
--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -8,7 +8,7 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
-  let app = new EmberApp(defaults, {
+  const app = new EmberApp(defaults, {
     // Add options here
     autoImport: {
       watchDependencies: ['@lineal-viz/lineal'],
