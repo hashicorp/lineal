76df747f699b0b8a01508dbe5c8262e46bb89fbe
Michael Lange <dingoeatingfuzz@gmail.com>
2023-06-12 17:26:07 -0700

    Example of live-updating line chart

--- test-app/app/controllers/lines.ts
+++ test-app/app/controllers/lines.ts
@@ -6,9 +6,41 @@
 import Controller from '@ember/controller';
 import { tracked, cached } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { ScaleLinear } from '@lineal-viz/lineal/scale';
+import { extent } from 'd3-array';
+
+const wiggle = (x: number) => x + Math.random() * x * 0.15 - x * 0.075;
 
 export default class LinesController extends Controller {
   @tracked activeDatum = null;
+  @tracked populationX = this.population;
+
+  constructor(...args: any[]) {
+    super(...args);
+
+    setInterval(() => {
+      console.log('beep');
+      console.log(this.population);
+      this.populationX = this.population.map((d) => ({
+        ...d,
+        people: wiggle(d.people as number),
+      }));
+    }, 100);
+  }
+
+  get pXScale() {
+    return new ScaleLinear({
+      domain: extent(this.populationX, (d) => +d.year).map((d) => d ?? 0),
+    });
+  }
+
+  get pYScale() {
+    return new ScaleLinear({
+      domain: extent(this.populationX, (d) => d.people as number).map(
+        (d) => d ?? 0
+      ),
+    });
+  }
 
   get population() {
     const data = this.model as any[];
